021c0d28a88268282eb9b1638e0d59e196b29c5f
P Czech <webcoderc@gmail.com>
2020-02-06 17:30:39 +0100

    Created method with test to save car or update report list when such exist

--- src/main/java/com/company/Parking/repository/CarRepository.java
+++ src/main/java/com/company/Parking/repository/CarRepository.java
@@ -2,8 +2,14 @@ package com.company.Parking.repository;
 
 import com.company.Parking.model.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CarRepository extends JpaRepository<Car, Long> {
+    Optional<Car> findAllByRegTable(String regTable);
 }
