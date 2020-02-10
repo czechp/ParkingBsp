fbbfdf390d1f58b85c0df1765f16f1b6473f4938
P Czech <webcoderc@gmail.com>
2020-02-10 17:31:00 +0100

    Add view to select registeration table

--- src/main/java/com/company/Parking/controller/CarController.java
+++ src/main/java/com/company/Parking/controller/CarController.java
@@ -14,6 +14,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Pattern;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/")
@@ -50,6 +53,16 @@ public class CarController {
         else if (!carService.createCar(car, new Report("Unknown")))
             return "entry_already_added";
         return "redirect:/";
+    }
+
+    @GetMapping("/car_delete")
+    public ModelAndView carDelete(){
+        ModelAndView modelAndView = new ModelAndView("get_car_to_delete");
+        List<@Pattern(regexp = "^[A-Z,0-9]{7}$") String> regs = carService.findAllCar().stream()
+                .map(Car::getRegTable)
+                .collect(Collectors.toList());
+        modelAndView.addObject("regs", regs);
+        return modelAndView;
     }
 
 }
