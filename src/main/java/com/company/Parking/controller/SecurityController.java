3c1f5b12628fa8319baaf3913e696cb7a94c4a4f
P Czech <webcoderc@gmail.com>
2020-02-26 17:45:34 +0100

    Customized registeration formss

--- src/main/java/com/company/Parking/controller/SecurityController.java
+++ src/main/java/com/company/Parking/controller/SecurityController.java
@@ -1,7 +1,12 @@
 package com.company.Parking.controller;
 
+import com.company.Parking.model.AppUser;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class SecurityController {
@@ -10,4 +15,17 @@ public class SecurityController {
     public String login(){
         return "Security/login";
     }
+
+    @GetMapping("/register_form")
+    public ModelAndView registerForm(){
+        return new ModelAndView("Security/register_form", "user", new AppUser());
+    }
+
+    @PostMapping("/register")
+    public String register(@ModelAttribute AppUser user){
+        System.out.println(user);
+        return "redirect:/register_form";
+    }
+
+
 }
